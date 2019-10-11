e5bbb7112f38169263052020ba76351031fd0578
blackyangA <931395490@qq.com>
2019-10-11 22:07:17 +0800

    first commit !

---
+++ demo01.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 21:58
+# @Software: PyCharm
+# @author: 洋
+# @File : demo01.py
+for i in range(10):
+    print(i)
---
+++ demo02.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/10/11 21:59
+# @Software: PyCharm
+# @author: 洋
+# @File : demo02.py
+i = 0
+while (i < 10):
+    i += 1
+    print("Hello World !")
